ebf8d48fd11428afe3d9ab8e028d893435ac75ed
Caue Queiroz <cauenqueiroz@gmail.com>
2021-11-15 21:08:28 -0300

    Create more assembly programs

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,76 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(START)
+    // screenpos = RAM[SCREEN]
+    @SCREEN
+    D=A
+    @screenpos
+    M=D
+
+    // maxcount = 8192
+    @8192
+    D=A
+    @maxcount
+    M=D
+
+    // count = 0
+    @count
+    M=0
+
+    // color = 0 (0 means white, 1 means black)
+    @color
+    M=0
+
+(LISTEN)
+    @KBD
+    D=M
+
+    // if (kbd == 0) goto FILLWHITE
+    @FILLWHITE
+    D;JEQ
+
+    // otherwise, goto FILLBLACK
+    @FILLBLACK
+    0;JMP
+
+(FILLWHITE)
+    @color
+    M=0
+
+    @PAINT
+    0;JMP
+
+(FILLBLACK)
+    @color
+    M=-1
+
+    @PAINT
+    0;JMP
+
+(PAINT)
+    // if (screenpos == maxscreenpos) goto START
+    @count
+    D=M
+    @maxcount
+    D=D-M
+    @START
+    D;JEQ
+
+    // paint screen with saved color
+    @color
+    D=M
+    @screenpos
+    A=M
+    M=D
+
+    // updates screenpos
+    @screenpos
+    M=M+1
+
+    // updates count
+    @count
+    M=M+1
+
+    @PAINT
+    0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,48 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+    // x = R0
+    @R0
+    D=M
+    @x
+    M=D
+
+    // y = R1
+    @R1
+    D=M
+    @y
+    M=D
+
+    // count = 0
+    @count
+    M=0
+
+    // R2 = 0
+    @R2
+    M=0
+
+(LOOP)
+    // if (count - y) == 0, goto end
+    @count
+    D=M
+    @y
+    D=D-M
+    @END
+    D;JEQ
+
+    // R2 += x
+    @x
+    D=M
+    @R2
+    M=M+D
+
+    // count += 1
+    @count
+    M=M+1
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP
